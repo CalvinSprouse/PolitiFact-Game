cfa53aee852eb47240e6a0aaf8715a637782bf2b
calvin <sprousecal@gmail.com>
2021-03-12 11:40:37 -0800

    added main function and runner

---
+++ quiz_game.py
@@ -0,0 +1,6 @@
+# create a question getter
+# store questions in list
+# store answers in list of lists list[0] = ["question", "correct answers", "player answer"]
+# the user may answer on a slider
+# create "grading" function to give full points for correct, half points for in the right ballpark etc.
+# allow user to customize their quiz with question count options and endless mode
